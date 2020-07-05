9c8f2f041c72a94b6400d065d7a8d1e58b43f98a
Patrick Uhlmann <git@uhlme.ch>
2020-07-05 08:18:54 +0200

    Inital Spring Initializer
    Selections:
    - Gradle
    - Java
    - Spring Boot 2.4.0 (SNAPSHOT)
    - Packaging Jar
    - Java 14
    - Dependencies: Spring Web, Thymeleaf, Spring Boot DevTools

---
+++ src/main/java/ch/uhlme/springwebsample/SpringWebSampleApplication.java
@@ -0,0 +1,13 @@
+package ch.uhlme.springwebsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSampleApplication.class, args);
+	}
+
+}
